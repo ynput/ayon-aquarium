5f3c4c7ec57cfa659fb0201547749a7d749ae4fb
Tony Dorfmeister <tony.dorfmeister@bepic.studio>
2024-05-06 10:37:39 +0200

    reverts specific leech events

--- services/leecher/leecher/leecher.py
+++ services/leecher/leecher/leecher.py
@@ -28,43 +28,6 @@ def create_event_description(event):
     return "Received {topic} #{_key}".format(topic=event.topic, _key=event._key)
 
 
-def create_event_topic(event) -> Optional[str]:
-    """Returns AYON event topic from Aquarium event topic.
-    Aquarium Examples:
-        user.assigned
-        item.created.Task
-    Output Examples:
-        aq.leech.user.assigned
-        aq.leech.task.created
-    """
-    parts = event.topic.split(".")
-    parts = [p.lower() for p in parts]
-    log.debug(f"{parts = }")
-    supported_items = {
-        "asset",
-        "shot",
-        "sequence",
-        "episode",
-        "task",
-        "user",
-        "project",
-    }
-    cmd, item = None, None
-    for _item in supported_items:
-        if _item in parts:
-            item = _item
-            cmd = parts[1]  #! might not be supported by processor
-            break
-
-    if not all([cmd, item]):
-        return None
-
-    result = ["aq", "leech", item, cmd]
-    result = ".".join(result)
-    log.debug(f"Event topic created {result}")
-    return result
-
-
 def callback(event):
     """Callback function for Aquarium event listener."""
     if event.topic in IGNORED_AQ_TOPICS:
@@ -75,20 +38,16 @@ def callback(event):
         log.warning(f"Event topic not allowed: {event.topic}")
         return
 
-    ay_topic = create_event_topic(event)
-    if not ay_topic:
-        log.warning(f"Can't build ayon topic for: {event.topic}")
-        return
-
     if ayon_api.dispatch_event(
-        ay_topic,
+        "aquarium.leech",
         sender=ayon_api.ServiceContext.service_name,
         event_hash=event._key,
         description=create_event_description(event),
         payload=event.to_dict(),
     ):
-        log.info(f"Created event {ay_topic}")
-    log.warning(f"Failed to create event {ay_topic}")
+        log.info(f"Created event for topic: {event.topic}")
+    else:
+        log.warning(f"Failed to create event for topic: {event.topic}")
 
 
 def main():
