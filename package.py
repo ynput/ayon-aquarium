9990ba586c706eeec9d5443cac3d3850a2f7347d
Tony Dorfmeister <tony.dorfmeister@bepic.studio>
2024-05-08 16:31:42 +0200

    bumps version, adds services

--- package.py
+++ package.py
@@ -1,5 +1,11 @@
 name = "aquarium"
 title = "Aquarium"
-version = "0.0.3"
+version = "0.0.4-dev.1"
+client_dir = "ayon_aquarium"
+
+services = {
+    "leecher": {"image": f"ynput/ayon-aquarium-leecher:{version}"},
+    "processor": {"image": f"ynput/ayon-aquarium-processor:{version}"},
+}
 
 plugin_for = ["ayon_server"]
