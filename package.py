637ba8d05580178288f542e20a6bbcc1a749ec8d
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-10-02 15:47:49 +0200

    formatting fix

--- package.py
+++ package.py
@@ -6,8 +6,8 @@ client_dir = "ayon_aquarium"
 
 # Match Services names created by manage.ps1 or makefile
 services = {
-        "leecher": {"image": f"ghcr.io/fatfish-lab/ayon-aquarium-leecher:{version}"},
-        "processor": {"image": f"ghcr.io/fatfish-lab/ayon-aquarium-processor:{version}"}
-    }
+    "leecher": {"image": f"ghcr.io/fatfish-lab/ayon-aquarium-leecher:{version}"},
+    "processor": {"image": f"ghcr.io/fatfish-lab/ayon-aquarium-processor:{version}"}
+}
 
 plugin_for = ["ayon_server"]
